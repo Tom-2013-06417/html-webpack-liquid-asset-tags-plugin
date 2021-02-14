649ec16d32e5a1cf6b84b6e92f2fa8684c944d4e
Tom <airbreather7@gmail.com>
2021-02-14 21:49:23 +0800

    Initial fork

---
+++ index.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-param-reassign */
+/**
+ * Updates HTMLWebpackPlugin assets src/href attributes to Shopify Liquid, piping
+ * the filename to `asset_url`.
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// eslint-disable-next-line no-empty-function
+function AssetTagToShopifyLiquid() {}
+
+AssetTagToShopifyLiquid.prototype.apply = (compiler) => {
+  compiler.hooks.compilation.tap('Assets Tag Plugin', (compilation) => {
+    // https://github.com/jantimon/html-webpack-plugin#events
+    compilation.hooks.htmlWebpackPluginAlterAssetTags.tap(
+      'AssetTagToShopifyLiquid',
+      (data) => {
+        function fixTag(tag) {
+          if (tag.tagName === 'script') {
+            tag.attributes.src = `{{ '${path.basename(
+              tag.attributes.src,
+            )}' | asset_url  }}`;
+          }
+
+          if (tag.tagName === 'link') {
+            tag.attributes.href = `{{ '${path.basename(
+              tag.attributes.href,
+            )}' | asset_url  }}`;
+          }
+
+          return tag;
+        }
+
+        data.head = data.head.map(fixTag);
+        data.body = data.body.map(fixTag);
+
+        return data;
+      },
+    );
+  });
+};
+
+module.exports = AssetTagToShopifyLiquid;
